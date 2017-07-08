41e8560e879ff71bc623cf4f932f69c8ace8623e
Justin Yoo <justin.yoo@aliencube.com>
2017-07-08 13:45:41 +1000

    Add mock API endpoints

---
+++ src/Sample.FunctionApp/MockTemplateDirectoriesHttpTriggerNotFound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+namespace Sample.FunctionApp
+{
+    /// <summary>
+    /// This represents the HTTP trigger entity for the MockTemplateDirectories event returning <see cref="HttpStatusCode.NotFound"/>.
+    /// </summary>
+    public static class MockTemplateDirectoriesHttpTriggerNotFound
+    {
+        /// <summary>
+        /// Invokes the HTTP trigger.
+        /// </summary>
+        /// <param name="req"><see cref="HttpRequestMessage"/> instance.</param>
+        /// <param name="log"><see cref="ILogger"/> instance.</param>
+        /// <returns>Returns the <see cref="HttpResponseMessage"/> instance.</returns>
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, ILogger log)
+        {
+            var models = new List<object>();
+
+            return req.CreateResponse(HttpStatusCode.NotFound, models);
+        }
+    }
+}
---
+++ src/Sample.FunctionApp/MockTemplateDirectoriesHttpTriggerOk.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+using Sample.Models.GitHub;
+
+namespace Sample.FunctionApp
+{
+    /// <summary>
+    /// This represents the HTTP trigger entity for the MockTemplateDirectories event returning <see cref="HttpStatusCode.OK"/>.
+    /// </summary>
+    public static class MockTemplateDirectoriesHttpTriggerOk
+    {
+        /// <summary>
+        /// Invokes the HTTP trigger.
+        /// </summary>
+        /// <param name="req"><see cref="HttpRequestMessage"/> instance.</param>
+        /// <param name="log"><see cref="ILogger"/> instance.</param>
+        /// <returns>Returns the <see cref="HttpResponseMessage"/> instance.</returns>
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, ILogger log)
+        {
+            var formatter = new JsonMediaTypeFormatter()
+                                {
+                                    SerializerSettings = new JsonSerializerSettings()
+                                                             {
+                                                                 ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                                                                 NullValueHandling = NullValueHandling.Ignore,
+                                                                 MissingMemberHandling = MissingMemberHandling.Ignore
+                                                             }
+                                };
+            var models = new List<ContentModel>()
+                             {
+                                 new ContentModel() { Name = "abc", Url = "https://templates.io/abc" },
+                                 new ContentModel() { Name = "xya", Url = "https://templates.io/xyz" }
+                             };
+
+            return req.CreateResponse(HttpStatusCode.OK, models, formatter);
+        }
+    }
+}
